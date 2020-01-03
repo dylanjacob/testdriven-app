180ea8918414e73ae3a40a6d9cfa3a78ee38a011
Dylan Jacob <dylan.jacob@bubblecore.net>
2020-01-03 00:02:02 -0800

    Set up JWT and auth routes

---
+++ services/users/project/tests/test_auth.py
@@ -0,0 +1,248 @@
+# services/users/project/tests/test_auth.py
+
+
+import json
+import unittest
+
+from flask import current_app
+from project import db
+from project.api.models import User
+from project.tests.base import BaseTestCase
+from project.tests.utils import add_user
+
+
+class TestAuthBlueprint(BaseTestCase):
+    def test_user_registration(self):
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps({
+                    'username': 'justatest',
+                    'email': 'test@test.com',
+                    'password': '123456',
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully registered.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 201)
+
+    def test_user_registration_duplicate_email(self):
+        add_user('test', 'test@test.com', 'test')
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps({
+                    'username': 'michael',
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Sorry. That user already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_user_registration_duplicate_username(self):
+        add_user('michael', 'test@test.com', 'test')
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps({
+                    'username': 'michael',
+                    'email': 'test@test2.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Sorry. That user already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_user_registration_invalid_json(self):
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps({}),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_user_registration_invalid_json_keys_no_username(self):
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_user_registration_invalid_json_keys_no_email(self):
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps({
+                    'username': 'michael',
+                    'password': 'test'
+                }),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_user_registration_invalid_json_keys_no_password(self):
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps({
+                    'username': 'michael',
+                    'email': 'test@test.com'
+                }),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_registered_user_login(self):
+        add_user('test', 'test@test.com', 'test')
+        with self.client:
+            response = self.client.post(
+                '/auth/login',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.content_type, 'application/json')
+            self.assertTrue(data['auth_token'])
+            self.assertIn('success', data['status'])
+            self.assertIn('Successfully logged in.', data['message'])
+
+    def test_not_registered_user_login(self):
+        with self.client:
+            response = self.client.post(
+                '/auth/login',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(response.content_type, 'application/json')
+            self.assertIn('fail', data['status'])
+            self.assertIn('User does not exist.', data['message'])
+
+    def test_valid_logout(self):
+        add_user('test', 'test@test.com', 'test')
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['auth_token']
+            response = self.client.get(
+                '/auth/logout',
+                headers={'Authorization': f'Bearer {token}'}
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Successfully logged out.', data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_invalid_logout_expired_token(self):
+        add_user('test', 'test@test.com', 'test')
+        current_app.config['TOKEN_EXPIRATION_SECONDS'] = -1
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['auth_token']
+            response = self.client.get(
+                '/auth/logout',
+                headers={'Authorization': f'Bearer {token}'}
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertIn('Signature expired. Please log in again.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_invalid_logout(self):
+        response = self.client.get(
+            '/auth/logout',
+            headers={'Authorization': f'Bearer invalid'}
+        )
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 401)
+        self.assertIn('Invalid token. Please log in again.', data['message'])
+        self.assertIn('fail', data['status'])
+
+    def test_user_status(self):
+        add_user('test', 'test@test.com', 'test')
+        with self.client:
+            resp_login = self.client.post(
+                '/auth/login',
+                data=json.dumps({
+                    'email': 'test@test.com',
+                    'password': 'test'
+                }),
+                content_type='application/json'
+            )
+            token = json.loads(resp_login.data.decode())['auth_token']
+            response = self.client.get(
+                '/auth/status',
+                headers={'Authorization': f'Bearer {token}'}
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['status'], 'success')
+            self.assertTrue(data['data'] is not None)
+            self.assertEqual(data['data']['username'], 'test')
+            self.assertEqual(data['data']['email'], 'test@test.com')
+            self.assertTrue(data['data']['active'] is True)
+            self.assertEqual(response.status_code, 200)
+
+    def test_invalid_status(self):
+        with self.client:
+            response = self.client.get(
+                '/auth/status',
+                headers={'Authorization': 'Bearer invalid'}
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertIn('fail', data['status'])
+            self.assertIn('Invalid token. Please log in again', data['message'])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -26,7 +26,8 @@ class TestUserService(BaseTestCase):
                 '/users',
                 data=json.dumps({
                     'username': 'michael',
-                    'email': 'michael@mherman.org'
+                    'email': 'michael@mherman.org',
+                    'password': 'greaterthaneight'
                 }),
                 content_type='application/json',
             )
@@ -55,7 +56,22 @@ class TestUserService(BaseTestCase):
         with self.client:
             response = self.client.post(
                 '/users',
-                data=json.dumps({'email': 'michael@mherman.org'}),
+                data=json.dumps({'email': 'michael@mherman.org', 'password': 'greaterthaneight'}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys_no_password(self):
+        """
+        Ensure error is thrown if the JSON object does not have a password key.
+        """
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({'username': 'michael', 'email': 'michael@mherman.org'}),
                 content_type='application/json',
             )
             data = json.loads(response.data.decode())
@@ -70,7 +86,8 @@ class TestUserService(BaseTestCase):
                 '/users',
                 data=json.dumps({
                     'username': 'michael',
-                    'email': 'michael@mherman.org'
+                    'email': 'michael@mherman.org',
+                    'password': 'greaterthaneight'
                 }),
                 content_type='application/json',
             )
@@ -78,7 +95,8 @@ class TestUserService(BaseTestCase):
                 '/users',
                 data=json.dumps({
                     'username': 'michael',
-                    'email': 'michael@mherman.org'
+                    'email': 'michael@mherman.org',
+                    'password': 'greaterthaneight'
                 }),
                 content_type='application/json',
             )
@@ -90,7 +108,7 @@ class TestUserService(BaseTestCase):
 
     def test_single_user(self):
         """Ensure get single user behaves correctly."""
-        user = add_user('michael', 'michael@mherman.org')
+        user = add_user('michael', 'michael@mherman.org', 'greaterthaneight')
         with self.client:
             response = self.client.get(f'/users/{user.id}')
             data = json.loads(response.data.decode())
@@ -119,8 +137,8 @@ class TestUserService(BaseTestCase):
 
     def test_all_users(self):
         """Ensure get all users behaves correctly."""
-        add_user('michael', 'michael@mherman.org')
-        add_user('fletcher', 'fletcher@notreal.com')
+        add_user('michael', 'michael@mherman.org', 'greaterthaneight')
+        add_user('fletcher', 'fletcher@notreal.com', 'greaterthaneight')
         with self.client:
             response = self.client.get('/users')
             data = json.loads(response.data.decode())
@@ -145,8 +163,8 @@ class TestUserService(BaseTestCase):
     def test_main_with_users(self):
         """Ensure the main route behaves correctly when users have been
         added to the database."""
-        add_user('michael', 'michael@mherman.org')
-        add_user('fletcher', 'fletcher@notreal.com')
+        add_user('michael', 'michael@mherman.org', 'greaterthaneight')
+        add_user('fletcher', 'fletcher@notreal.com', 'greaterthaneight')
         with self.client:
             response = self.client.get('/')
             self.assertEqual(response.status_code, 200)
@@ -162,7 +180,7 @@ class TestUserService(BaseTestCase):
         with self.client:
             response = self.client.post(
                 '/',
-                data={'username': 'michael', 'email': 'michael@sonotreal.com'},
+                data={'username': 'michael', 'email': 'michael@sonotreal.com', 'password': 'greaterthaneight'},
                 follow_redirects=True
             )
             self.assertEqual(response.status_code, 200)
