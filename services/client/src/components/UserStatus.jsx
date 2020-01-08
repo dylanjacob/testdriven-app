0c2d1a755446c1daa160540a64ac8b20c56a645c
Dylan Jacob <dylan.jacob@bubblecore.net>
2020-01-07 22:38:59 -0800

    Added UserStatus

---
+++ services/client/src/components/UserStatus.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+class UserStatus extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            id: ''
+        };
+    };
+    componentDidMount() {
+        if (this.props.isAuthenticated) {
+            this.getUserStatus();
+        }
+    };
+    getUserStatus(event) {
+        const options = {
+            url: `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/status`,
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${window.localStorage.authToken}`
+            }
+        };
+        return axios(options)
+            .then((res) => {
+                this.setState({
+                    email: res.data.data.email,
+                    username: res.data.data.username,
+                    id: res.data.data.id,
+                })
+            })
+            .catch((err) => { console.log(err); });
+    };
+    render() {
+        if (!this.props.isAuthenticated) {
+            return(
+                <p>You must be logged in to view this. Click <Link to='/login'>here</Link> to log in.</p>
+            )
+        };
+        return(
+            <div>
+                <ul>
+                    <li><strong>User Id:</strong> {this.state.id}</li>
+                    <li><strong>Email:</strong> {this.state.email}</li>
+                    <li><strong>Username:</strong> {this.state.username}</li>
+                </ul>
+            </div>
+        )
+    };
+}
+
+export default UserStatus;
