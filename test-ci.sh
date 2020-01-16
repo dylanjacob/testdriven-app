f189fc97fca1ab64d54f5fa455b9a7358b5d360f
Dylan Jacob <dylan.jacob@bubblecore.net>
2020-01-15 23:45:10 -0800

    added a new CI test script and updated docker build script

--- test-ci.sh
+++ test-ci.sh
@@ -12,5 +12,41 @@ inspect() {
 # run client and server-side tests
 dev() {
   docker-compose up -d --build
-  docker-compose exec users python
+  docker-compose exec users python manage.py test
+  inspect $? users
+  docker-compose users flake8 project
+  inspect $? users-lint
+  docker-compose exec client npm test -- --coverage -u
+  inspect $? client
+  docker-compose down
 }
+
+e2e() {
+  docker-compose -f docker-compose-$1.yml up -d --build
+  docker-compose -f docker-compose-$1.yml run users python manage.py recreate_db
+  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+  inspect $? e2e
+  docker-compose -f docker-compose-$1.yml down
+}
+
+if [[ "${env}" == "development" ]]; then
+  echo "Running client and server-side tests!"
+  dev
+elif [[ "${env}" == "staging" ]]; then
+  echo "Running e2e tests!"
+  e2e stage
+elif [[ "${env}" == "production" ]]; then
+  echo "Running e2e tests!"
+  e2e prod
+else
+  echo "Running client and server-side tests!"
+  dev
+fi
+
+if [ -n "${fails}" ]; then
+  echo "Tests failed: ${fails}"
+  exit 1
+else
+  echo "Tests passed!"
+  exit 0
+fi
